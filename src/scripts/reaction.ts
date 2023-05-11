d1a1635743ed2605c09dac17278a8e0275f88880
Hellow2 <lars.noack@outlook.de>
2023-05-11 14:22:00 +0200

    implemented the molecule class in typescript

--- src/scripts/reaction.ts
+++ src/scripts/reaction.ts
@@ -14,6 +14,16 @@ const SUBSCRIPT_MAP: {[key: string]: string} = {
 };
 
 
+function defaultDict(defaultValue: any) {
+    return new Proxy({}, {
+      get: (target, name) => name in target ? target[name] : defaultValue
+    });
+}
+
+
+class ParsingError extends Error {}
+
+
 class Atom {
     name: string;
     count: number
@@ -36,4 +46,57 @@ class Atom {
 
         return this.name + subscript;
     }
+}
+
+
+class Molecule {
+    coefficient: number = 1;
+    atomList: Atom[] = [];
+
+    id: string;
+    side: number;
+
+    private elementSet: Set<string> = new Set();
+    private atomMap: {[key: string]: number} = defaultDict(0);
+
+    constructor(id: string, side: number) {
+        this.id = id;
+        this.side = side;
+    }
+
+    toString(): string {
+        if (this.coefficient === 0) return "";
+
+        const atomString = this.atomList.map(atom => atom.toString()).join("");
+
+        if (this.coefficient === 1) return atomString;
+        else this.coefficient.toString() + atomString;
+    }
+
+    setCoeficient(coefficient: number): void {
+        if (this.atomList.length === 0) this.coefficient *= coefficient;
+        else this.atomList[this.atomList.length - 1].count = coefficient;
+    }
+
+    addAtom(name: string): void {
+        this.atomList.push(new Atom(name, 1));
+    }
+
+    modifyAtomName(name: string): void {
+        if (this.atomList.length === 0) throw ParsingError;
+
+        this.atomList[this.atomList.length - 1].name += name
+    }
+
+    mapAtom(): void {
+        for (const atom of this.atomList) {
+            this.atomMap[atom.name] = atom.count;
+            this.elementSet.add(atom.name);
+        }
+    }
+
+    getAtomCount(element: string): number {
+        return this.atomMap[element] * this.coefficient * this.side;
+    }
+
 }
